5a1fef7760dfeac58a883e81d1b8a1063fe79c96
AnasAL21 <anasaitlhaj2108@gmail.com>
2023-09-13 12:46:34 -0700

    update

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = matrix.copy()
-for row in new_matrix:
-    squared_row = []
-    for element in row:
-        squared_element = element ** 2
-
-    squared_row.append(squared_element)
-    new_matrix.append(squared_row)
-return new_matrix
+    new_matrix = []
+    for row in matrix:
+        new_row = []
+        for item in row:
+            new_row.append(item * item)
+        new_matrix.append(new_row)
+    return new_matrix
