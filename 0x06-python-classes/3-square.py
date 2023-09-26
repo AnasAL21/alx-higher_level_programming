8538e2496e699709109ab49ea9dee6eb7a1069e1
AnasAL21 <anasaitlhaj2108@gmail.com>
2023-09-26 11:01:42 -0700

    update

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,17 @@
+class Square:
+    """Representing square."""
+
+    def __init__(self, size=0):
+        """Initializing new square.
+
+        Args:
+            size (int): size of a new square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return (self.__size * self.__size)
