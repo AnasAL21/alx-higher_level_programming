766f008cf8b5a794977bbfac597fbc2ca703bd2e
AnasAL21 <anasaitlhaj2108@gmail.com>
2023-09-26 10:08:39 -0700

    update

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,10 +3,12 @@
 
 class Square:
     """Represent a square."""
+    
     def __init__(self, size):
          """Initializing a new Square.
 
         Args:
             size (int): Size of new square.
         """
+        
         self.__size = size
