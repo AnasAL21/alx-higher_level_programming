2722e317cd64983cd92aa1b2ba1681c4ebb76e7a
AnasAl21 <anasaitlhaj2108@gmail.com>
2023-10-02 22:50:41 +0000

    update

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -2,14 +2,14 @@
 """Defines a Rectangle class."""
 
 class Rectangle:
-    """Represent a Rectangle."""
+    """Represent a rectangle."""
 
     def __init__(self, width=0, height=0):
-        """Initialize a Rectangle.
+        """Initialize a new Rectangle.
 
         Args:
-            width (int): the measure of how wide an object is.
-            height (int): the measure of how long an object is.
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
         """
         self.width = width
         self.height = height
@@ -29,7 +29,7 @@ class Rectangle:
 
     @property
     def height(self):
-        """Get or set the height of the Rectangle."""
+        """Get/set the height of the Rectangle."""
         return self.__height
 
     @height.setter
@@ -41,19 +41,19 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        """Return the area """
+        """Return the area of the Rectangle."""
         return (self.__width * self.__height)
 
     def perimeter(self):
-        """Return the perimeter """
+        """Return the perimeter of the Rectangle."""
         if self.__width == 0 or self.__height == 0:
             return (0)
         return ((self.__width * 2) + (self.__height * 2))
 
     def __str__(self):
-        """Return the printable representation.
+        """Return the printable representation of the Rectangle.
 
-        Represents the rectangle with the # .
+        Represents the rectangle with the # character.
         """
         if self.__width == 0 or self.__height == 0:
             return ("")
