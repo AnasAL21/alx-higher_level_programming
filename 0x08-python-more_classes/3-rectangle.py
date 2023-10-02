bba853a3b36e434f7d5611d2230737db6da0967e
AnasAl21 <anasaitlhaj2108@gmail.com>
2023-10-02 21:06:14 +0000

    update

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -47,6 +47,7 @@ class Rectangle:
         return ((self.__width * 2) + (self.__height * 2))
 
     def __str__(self):
+        """represent rectangle with #."""
         if self.__width == 0 or self.__height == 0:
             return ("")
 
