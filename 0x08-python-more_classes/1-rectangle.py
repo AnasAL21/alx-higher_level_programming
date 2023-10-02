403f17ec5994aea945fdca2f95c068acb0a368c7
AnasAl21 <anasaitlhaj2108@gmail.com>
2023-10-02 18:12:51 +0000

    update

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -10,27 +10,29 @@ class Rectangle:
             width (int): the measure of how wide an object is.
             lenght (int): the measure of how long an object is. 
         """
-        self.width = w
-        self.lenght = l
+        self.width = wigth
+        self.lenght = lenght
         
     @property
     def width(self):
-        return self.width
+        return self.__width
     
     @width.setter
-    def width(self, w):
-        if not isinstance(w, int):
+    def width(self, value):
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif w < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
-        
+        self.__width = value
+    
     @property
     def height(self):
-        return self.height
+        return self.__lenght
     
     @height.setter
-    def height(self, l):
-        if not isinstance(l, int):
-            raise TabError("height must be an intger")
-        elif l < 0:
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an intger")
+        elif value < 0:
             raise ValueError("height must be >= 0")
+        self.__lenght = value
